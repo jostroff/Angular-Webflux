45a448cdab47d0312346eedac3e9899969965031
JOstroff <JOstroff@L000905398.EHEALTH.HPHC.ORG>
2020-10-02 11:43:33 -0400

    It works!

--- src/app/app/app.component.ts
+++ src/app/app/app.component.ts
@@ -6,10 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  title: string;
-
-  constructor() {
-    this.title = 'Spring Boot - Angular Application';
-  }
+  title = 'angular-client10';
 }
